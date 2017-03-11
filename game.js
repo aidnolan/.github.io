029aed6db66c55c6ec2598820ce5315372a68bed
aidnolan <aidnolan@hotmail.com>
2017-03-11 00:17:44 +0000

    Update to game, addition of blank js file

--- game.js
+++ game.js
@@ -1,68 +1,97 @@
-alert("Welcome to the game!");
-
-var invalid = true;
-
-function firstCap(str) {
-    var firstLetter = str.substr(0, 1);
-    return firstLetter.toUpperCase() + str.substr(1);
+function Animals (animal, name, action) {
+	this.animal = animal;
+	this.name = name;
+	this.action = action;
 }
 
-while(invalid === true) {
-    var animal  = prompt("Please choose your animal:\n Fox / Dog / Cat / Sloth").toLowerCase();
-    switch(animal) {
-        case "fox":
-			alert("You've picked the wiley fox!");
-			invalid = false;
-			break;
-		
-		case "dog":
-			alert("You've picked the lazy dog!");
-			invalid = false;
-			break;
-		
-		case "cat":
-			alert("You've picked the cranky cat!");
-			invalid = false;
-			break;
-		
-		case "sloth":
-			alert("You've picked the slinky sloth!");
-			invalid = false;
-			break;
-		
-		default:
-			alert("Please choose one of the animals specified with no spaces or other symbols");
-	}
+var slothID = document.getElementById("sloth");
+var inputName = document.getElementById("typedInput").value;
+var nameString = document.getElementById("typedBox")
+
+var sloth = new Animals("sloth", inputName, "slinky ",);
+var fox = new Animals("fox", inputName, "slinky ");
+var dog = new Animals("dog", inputName, "lazy ");
+var cat = new Animals("cat", inputName, "cranky ");
+
+
+function alertMe (animal) {
+	alert('You have chosen the ' + this.action + this.animal)
 }
 
-var name = prompt("Please choose a name for your " + animal + ": ");
-name = firstCap(name);
-
-alert("It's nice to meet you " + name + "!");
 
 
-while (invalid === false) {
-	var action = prompt("What would you like to do today? \n1) Scratch yourself \n2) Eat some marshmallows \n3) Tell everybody what you really think of them");
-    switch(action) {
+slothID.onclick = alertMe;
+
+
+// function Animals = {
+// 	this.animal = animal;
+
+
+// x("Welcome to the game!");
+
+// var invalid = true;
+
+// function firstCap(str) {
+//     var firstLetter = str.substr(0, 1);
+//     return firstLetter.toUpperCase() + str.substr(1);
+// }
+
+// while(invalid === true) {
+//     var animal  = prompt("Please choose your animal:\n Fox / Dog / Cat / Sloth").toLowerCase();
+//     switch(animal) {
+//         case "fox":
+// 			x("You've picked the wiley fox!");
+// 			invalid = false;
+// 			break;
 		
-		case "1":
-			alert("That's it " + name + ", you give yourself a nice long scratch in all those places you're not allowed to in public");
-			invalid = true;
-		break;
+// 		case "dog":
+// 			x("You've picked the lazy dog!");
+// 			invalid = false;
+// 			break;
 		
-		case "2":
-			alert("Don't eat them all at once " + name + "! Oh no... too late...");
-			invalid = true;
-		break;
+// 		case "cat":
+// 			x("You've picked the cranky cat!");
+// 			invalid = false;
+// 			break;
+		
+// 		case "sloth":
+// 			x("You've picked the slinky sloth!");
+// 			invalid = false;
+// 			break;
+		
+// 		default:
+// 			x("Please choose one of the animals specified with no spaces or other symbols");
+// 	}
+// }
 
-		case "3":
-			alert("Whoa, you told 'em " + name + " but did you have to poke your " + animal + " claws in all of their faces like that?");
-			invalid = true;
-		break;
+// var name = prompt("Please choose a name for your " + animal + ": ");
+// name = firstCap(name);
 
-		default:
-			alert("Sorry " + name + " but that sort of behaviour is off the menu!");
-	}
-}
+// x("It's nice to meet you " + name + "!");
 
-console.log(invalid);
+
+// while (invalid === false) {
+// 	var action = prompt("What would you like to do today? \n1) Scratch yourself \n2) Eat some marshmallows \n3) Tell everybody what you really think of them");
+//     switch(action) {
+		
+// 		case "1":
+// 			x("That's it " + name + ", you give yourself a nice long scratch in all those places you're not allowed to in public");
+// 			invalid = true;
+// 		break;
+		
+// 		case "2":
+// 			x("Don't eat them all at once " + name + "! Oh no... too late...");
+// 			invalid = true;
+// 		break;
+
+// 		case "3":
+// 			x("Whoa, you told 'em " + name + " but did you have to poke your " + animal + " claws in all of their faces like that?");
+// 			invalid = true;
+// 		break;
+
+// 		default:
+// 			x("Sorry " + name + " but that sort of behaviour is off the menu!");
+// 	}
+// }
+
+// console.log(invalid);
--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
 	$('h1').fadeIn('slow');
  });
-//     $('h1').click(function() {
-//         $(this).slideUp(1000).slideDown(1000);
-//     });
-// });
+    // $('h1').click(function() {
+    //     $(this).slideUp(1000).slideDown(1000);
+    // }));
 
-$('.button').click(function() {
-      $('.button').addClass('buttonclicked');
+$('#button1').click(function() {
+    $('#button1').addClass("#button1 buttonclicked");
 
-	setTimeout(function () { 
-    $('.button').removeClass('buttonclicked');
+    setTimeout(function () { 
+    $('#button1').removeClass("#button1 buttonclicked");
 	}, 100);
     $('p').slideToggle(500);
     }); 
+// function alertMe () {
+// 	alert('TEST');
+// }
 
-
-  
+// document.getElementById("button1").addEventListener("click", alertMe);
